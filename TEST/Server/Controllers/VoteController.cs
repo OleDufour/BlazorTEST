1528acc9d901d54cd4141c9fac67239e75fca102
Ole Dufour <ole.dufour.ucf@cibtp.fr>
2020-05-22 17:41:05 +0200

    Ajouter une vote (proposition)

--- TEST/Server/Controllers/VoteController.cs
+++ TEST/Server/Controllers/VoteController.cs
@@ -3,25 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using TEST.Server.Models;
 using TEST.Shared;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace TEST.Server.Controllers
 {
-    // [AllowAnonymous]
-    [Authorize]
     [ApiController]
     [Route("[controller]/[action]")]
-    public class DossierController : ControllerBase
+    public class VoteController :MonConciergeControllerBase
     {
         private readonly MonConciergeContext _context;
 
-        public DossierController(MonConciergeContext context)
+        public VoteController(MonConciergeContext context)
         {
             _context = context;
 
@@ -29,37 +25,54 @@ namespace TEST.Server.Controllers
 
         // GET: api/<ValuesController>
         [HttpGet]
-        public async Task<List<Dossier>> GetDossiers()
+        public List<Vote> GetPropositions()
         {
-            List<Dossier> v = await _context.Dossier.ToListAsync();
-            return v;
+            IQueryable<Vote> v = _context.Vote.Select(x => x);
+            return v.ToList();
         }
 
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
-        public Dossier GetDossier(int id)
+        public string Get(int id)
         {
-            Dossier d = _context.Dossier.Include(b => b.Vote).Where(x => x.Id == id).Single();//.Select(x => x);
-            return d;
+            return "value";
         }
 
 
         [HttpPost]
         //[Route("Create")]
-        public async Task<IActionResult> Add(Dossier dossier)
+        public async Task<IActionResult> Add(Vote proposition)
         {
             var response = new ResponseSingle<int>();
             ClaimsPrincipal currentUser = this.User;
             var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            dossier.CreationDate = DateTime.Now;
-            dossier.UserId = currentUserID;
 
-            _context.Add(dossier);
+            _context.Add(proposition);
+            proposition.CreationDate = DateTime.Now;
+            proposition.UserId = currentUserID;
+
+
+
+
 
             await _context.SaveChangesAsync();
+            //      return NoContent();
             return Ok(response);
         }
+
+
+        [HttpPost]
+        //[Route("Create")]
+        public async Task<IActionResult> Cast(Vote vote)
+        {
+            var response = new ResponseSingle<int>();
+            _context.Add(vote);
+            await _context.SaveChangesAsync();
+            //      return NoContent();
+            return Ok(response);
+        }
+
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
---
+++ TEST/Server/MonConciergeControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEST.Server
+{
+    [Authorize]
+    public class MonConciergeControllerBase : ControllerBase
+    {
+        public ClaimsPrincipal CurrentUser { get; private set; }
+        public string CurrentUserID { get; private set; }
+        public MonConciergeControllerBase()
+        {
+            //CurrentUser = this.User;
+            //CurrentUserID = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}

3869714926eefce83f4f825f41c2241e7d177203
Ole Dufour <ole.dufour.ucf@cibtp.fr>
2020-05-24 18:14:28 +0200

    Added a link Merci de voter in proposition list

---
+++ TEST/Shared/DBModelPartials/Proposition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TEST.Server
+{
+    public partial class Proposition
+    {
+        [NotMapped]
+        public bool CurrentUserHasVoted { get; set; }
+
+        //public Proposition()
+        //{
+        //    VoteCasted = new HashSet<VoteCasted>();
+        //}
+
+        //[Key]
+        //[Column("ID")]
+        //public int Id { get; set; }
+        //[Column(TypeName = "datetime")]
+        //public DateTime CreationDate { get; set; }
+        //[Required]
+        //[StringLength(100)]
+        //public string Title { get; set; }
+        //[Required]
+        //[StringLength(450)]
+        //public string Content { get; set; }
+        //[Required]
+        //[Column("UserID")]
+        //[StringLength(450)]
+        //public string UserId { get; set; }
+        //[Column(TypeName = "datetime")]
+        //public DateTime? ClosedDate { get; set; }
+        //[Column("DossierID")]
+        //public int DossierId { get; set; }
+
+        //[ForeignKey(nameof(DossierId))]
+        //[InverseProperty("Proposition")]
+        //public virtual Dossier Dossier { get; set; }
+        //[ForeignKey(nameof(UserId))]
+        //[InverseProperty(nameof(AspNetUsers.Proposition))]
+        //public virtual AspNetUsers User { get; set; }
+        //[InverseProperty("Proposition")]
+        //public virtual ICollection<VoteCasted> VoteCasted { get; set; }
+    }
+}

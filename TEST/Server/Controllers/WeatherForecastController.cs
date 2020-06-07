1337d4f71478db844e270a1b791a33902142e138
Ole Dufour <ole.dufour.ucf@cibtp.fr>
2020-06-07 13:46:46 +0200

    Dockerization

--- TEST/Server/Controllers/WeatherForecastController.cs
+++ TEST/Server/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace TEST.Server.Controllers
 {
-    [Authorize]
+   // [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
---
+++ TEST/Server/delete.sql
@@ -0,0 +1,5 @@
+ 
+  
+delete [MonConcierge].[dbo].voteCasted
+delete [MonConcierge].[dbo].[Proposition]
+delete [MonConcierge].[dbo].Dossier
